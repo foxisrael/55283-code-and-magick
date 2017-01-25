c01ad1ef59e5f97ad9722534e2d0ef7120f1f37b
Dmitriy Z <dmitriy.zamanskiy@gmail.com>
2017-01-25 13:09:53 +0300

    2 задание

--- js/stat.js
+++ js/stat.js
@@ -16,43 +16,27 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-//Обращаемся к массивам
-  //Не понял, что за переменная max. Для чего она?
-  var max = -1;
-
-  //Перебираем данные в массиве, вопрос почему только в массиве со временем?
-  for (var i = 0; i < times.length; i++) {
-    //Вот после этого я не понял...
+  var max = times[0];
+  for (var i = 1; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
     }
   }
 
-  //Тут рисуем гистограмму
-  //Высота гистограммы
   var histoHeight = 150;
-
-  //Не понял, что это такое
   var histoX = 120;
-
-  //Высота отдельных результатов
   var step = histoHeight / max;
-
-  //Ширина отдельных результатов
-  var columnIndent = 40;
-
-  //Опять ниже все не понял
+  var columnIndent = 100;
   for (var i = 0; i < times.length; i++) {
     var name = names[i];
     var time = times[i];
     var height = step * time;
 
-    ctx.fillStyle = '#000';
-
-    //Вот эти строки не понял совсем
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     ctx.fillRect(histoX + columnIndent * i, 100, 40, height);
-    ctx.fillText(name + ':' + time.toFixed(0), histoX + columnIndent * i, 100 + histoHeight + 20);
+    ctx.fillText(name, histoX + columnIndent * i, histoHeight - 60);
+    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 100 + histoHeight + 20);
   }
 };
 
