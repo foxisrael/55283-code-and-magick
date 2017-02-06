42f2157d6d5c1c8010bf0c375e4cd71d9ccf5559
Dmitriy Z <dmitriy.zamanskiy@gmail.com>
2017-02-06 14:46:54 +0200

    4 задание

--- js/setup.js
+++ js/setup.js
@@ -1,68 +1,64 @@
-'use strict';
-//Переменные открытия окна
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = document.querySelector('.setup-close');
+  'use strict';
 
-//Открытия и закрытия окна
-setupOpen.addEventListener('click', function() {
-  setup.classList.remove('invisible');
-});
+  // Переменные открытия окна
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = document.querySelector('.setup-close');
 
-setupClose.addEventListener('click', function() {
-  setup.classList.add('invisible');
-});
+  // Открытия и закрытия окна
+  setupOpen.addEventListener('click', function() {
+    setup.classList.remove('invisible');
+  });
 
-// //Валидация формы
-// var nameField = document.querySelector('.setup-user-name');
-// nameField.required = true;
-// nameField.maxlength = 50;
+  setupClose.addEventListener('click', function() {
+    setup.classList.add('invisible');
+  });
 
-//Переменные одежки и файерболла мага
-var wizard = document.querySelector('#wizard');
-var wizardCoat = wizard.querySelector('#wizard-coat');
-var wizardEyes = wizard.querySelector('#wizard-eyes');
-var wizardFireBall = document.querySelector('.setup-fireball-wrap');
+  // Переменные одежки и файерболла мага
+  var wizard = document.querySelector('#wizard');
+  var wizardCoat = wizard.querySelector('#wizard-coat');
+  var wizardEyes = wizard.querySelector('#wizard-eyes');
+  var wizardFireBall = document.querySelector('.setup-fireball-wrap');
 
-//Смена цвета плаща мага
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
+  // Смена цвета плаща мага
+  var wizardCoatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
 
-wizardCoat.addEventListener('click', function() {
-  var colorNumber = Math.floor(Math.random() * wizardCoatColors.length);
-  wizardCoat.style.fill = wizardCoatColors[colorNumber];
-});
+  wizardCoat.addEventListener('click', function() {
+    var colorNumber = Math.floor(Math.random() * wizardCoatColors.length);
+    wizardCoat.style.fill = wizardCoatColors[colorNumber];
+  });
 
-//Смена цвета глаз мага
-var wizardEyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
+ // Смена цвета глаз мага
+  var wizardEyesColors = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
 
-wizardEyes.addEventListener('click', function() {
-  var colorNumber = Math.floor(Math.random() * wizardEyesColors.length);
-  wizardEyes.style.fill = wizardCoatColors[colorNumber];
-});
+  wizardEyes.addEventListener('click', function() {
+    var colorNumber = Math.floor(Math.random() * wizardEyesColors.length);
+    wizardEyes.style.fill = wizardCoatColors[colorNumber];
+  });
 
-//Смена цвета файерболла мага
-var wizardFireBallColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
+  // Смена цвета файерболла мага
+  var wizardFireBallColors = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+   '#e6e848'
+  ];
 
-wizardFireBall.addEventListener('click', function() {
-  var colorNumber = Math.floor(Math.random() * wizardFireBallColors.length);
-  wizardFireBall.style.background = wizardFireBallColors[colorNumber];
-});
+  wizardFireBall.addEventListener('click', function() {
+    var colorNumber = Math.floor(Math.random() * wizardFireBallColors.length);
+    wizardFireBall.style.background = wizardFireBallColors[colorNumber];
+  });
